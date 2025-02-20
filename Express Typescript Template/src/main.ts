b34b131656dd0da3781486fe60d2e5aad0552bba
lloydgeorge0410 <lloydgeorge0410@outlook.com>
2025-02-20 15:21:38 -0800

    pw-13: implement environment variables and add custom logger

--- Express Typescript Template/src/main.ts
+++ Express Typescript Template/src/main.ts
@@ -1,8 +1,10 @@
 import app from "./app";
+import { ENV } from "./utils/constants";
+import Logger from "./utils/logger";
 
 const startServer = async () => {
-  app.listen(5000, "127.0.0.1", () => {
-    console.log(`Server is running at http://127.0.0.1:5000.`);
+  app.listen(ENV.PORT, ENV.HOST, () => {
+    Logger.info(`Server is running at http://${ENV.HOST}:${ENV.PORT}.`);
   });
 };
 
