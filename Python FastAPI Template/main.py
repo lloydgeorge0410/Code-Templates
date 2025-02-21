386bed82283fbaaba89a5753abc72dafb54025fe
lloydgeorge0410 <lloydgeorge0410@outlook.com>
2025-02-20 16:23:31 -0800

    pw-8: manage environment variables

--- Python FastAPI Template/main.py
+++ Python FastAPI Template/main.py
@@ -1,5 +1,13 @@
 import uvicorn
 
+from app.config.settings import settings
+from app.utils.constants import MODE
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", reload=True)
+    uvicorn.run(
+        "app.app:app",
+        reload=settings.mode == MODE.DEV,
+        port=settings.port,
+        host=settings.host,
+        workers=4 if settings.mode == MODE.PROD else None,
+    )
